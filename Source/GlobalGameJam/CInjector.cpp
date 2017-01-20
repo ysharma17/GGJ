5c1c9740dfa0c2e7791c9dd3df3514f1482f815e
JustinCamden <camden@usc.edu>
2017-01-20 14:16:09 -0800

    Initializing project

---
+++ Source/GlobalGameJam/CInjector.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GlobalGameJam.h"
+#include "CInjector.h"
+
+CInjector::CInjector()
+{
+}
+
+CInjector::~CInjector()
+{
+}
---
+++ Source/GlobalGameJam/GlobalGameJam.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GlobalGameJam.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GlobalGameJam, "GlobalGameJam" );
